6e7525ba134fcf972f82d6650ec271f8f2242d69
Mike <schaemik@students.zhaw.ch>
2016-11-13 16:43:46 +0100

    -Added CustomCameraView Class to the FaceRecognitionLibrary Helpers

---
+++ src/main/java/ch/zhaw/facerecognitionlibrary/Helpers/CustomCameraView.java
@@ -0,0 +1,12 @@
+package ch.zhaw.facerecognitionlibrary.Helpers;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.opencv.android.JavaCameraView;
+
+
+public class CustomCameraView extends JavaCameraView {
+
+    public CustomCameraView(Context context, AttributeSet attrs) { super(context, attrs);}
+}
